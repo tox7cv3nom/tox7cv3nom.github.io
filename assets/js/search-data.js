61f62eb783976244345ff811db400e1ef9c14da2
toxic v3nom <nhibtaungamain@gmail.com>
2021-07-05 08:09:27 -0400

    Update and rename search.js to search-data.js

---
+++ assets/js/search-data.js
@@ -0,0 +1,26 @@
+var store = [{
+        "title": "Diving in Aheap glibc Architecture",
+        "excerpt":"Hello everyone, This is my first blog post. Many of my friends suggested me to get into Android Security. I started learning android security and learned many things, but I faced a lot of problems. So I wanted to create a series of blog posts to guide those peeps who...","categories": ["Android Security"],
+        "tags": ["Exploit Development", "heap"],
+        "url": "https://tox7cv3nom.github.io/2021/06/13/heap-glibc-architecture.html"
+      },{
+        "title": "List of HTB Machine Walkthroughs",
+        "excerpt":"Hello to all of you! I hope you all are well in this pandemic. I started doing hackthebox machines; that’s why I created a list of hackthebox machine walkthroughs. I love to read/watch walkthroughs by 0xdf, ippsec, and xct. I will update this regularly. We all know that how much...","categories": ["Hack The Box"],
+        "tags": ["Writeups","Resources","Penetration Testing","Challenges"],
+        "url": "https://iamsarvagyaa.github.io/htb-writeups-list/"
+      },{
+        "title": "Think outside the box with Debangshu Kundu",
+        "excerpt":"I’m back again with a new series of posts named “Think outside the box,” inspired by PentesterLand’s AMA. I planned to interview good hackers, where I asked a few questions about his industry domains. This is the first post in this series of interviews, and I’m pleased about this initiative....","categories": ["Interview"],
+        "tags": ["AEM Security","Bug Bounty","API Security","AMA Session"],
+        "url": "https://iamsarvagyaa.github.io/think-outof-thebox-01/"
+      },{
+        "title": "Think outside the box with Satyam Gothi",
+        "excerpt":"Hey, all. I hope you all are well during this pandemic. First of all, I would like to thank all of you for loving the interview sessions. I got a wonderful response in the last interview. Today, I’m going to invite “Satyam Gothi aka RogueSMG”. He is a security analyst...","categories": ["Interview"],
+        "tags": ["Bug Bounty","API Security","AMA Session"],
+        "url": "https://iamsarvagyaa.github.io/think-outof-thebox-02/"
+      },{
+        "title": "Think outside the box with Devansh Batham",
+        "excerpt":"Hey, everyone. I hope you all are safe in this pandemic. After a long time, I am back with an interview session. So, today’s guest is Devansh Batham, aka Asm0d3us. He is currently working as a triager at HackerOne, and also he created a lot of tools. He gave wonderful...","categories": ["Interview"],
+        "tags": ["Bug Bounty","Unicode Normalization","AMA Session"],
+        "url": "https://iamsarvagyaa.github.io/think-outof-thebox-03/"
+      }]
